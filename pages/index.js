520005888b20af501814979e604a3c94447bfabe
sanjay <sanjay.varma@ufaber.com>
2022-10-18 14:06:43 +0530

    Loader

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 import React from "react";
 import { motion } from "framer-motion";
-import Loader from "../component/loader";
+
+import Loader from "../component/Loader";
 // import bg from "/images/background-image.svg";
 const index = () => {
 	return (
